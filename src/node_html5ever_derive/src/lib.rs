0cbec2722e4c94af5836bb56b9ff19729df34711
David Björklund <david.bjorklund@gmail.com>
2022-09-24 11:29:36 +0200

    cleanup, remove unused file

--- src/node_html5ever_derive/src/lib.rs
+++ src/node_html5ever_derive/src/lib.rs
@@ -12,7 +12,11 @@ pub fn add_node_fields(_args: TokenStream, input: TokenStream) -> TokenStream {
           fields.named.push(
             syn::Field::parse_named
               .parse2(quote! {
-                pub(crate) parent: Option<napi::Either<napi::bindgen_prelude::WeakReference<crate::element::Element>, napi::bindgen_prelude::WeakReference<crate::document::Document>>>
+                pub(crate) parent:
+                  Option<napi::Either<
+                    napi::bindgen_prelude::WeakReference<crate::element::Element>,
+                    napi::bindgen_prelude::WeakReference<crate::document::Document>
+                  >>
               })
               .unwrap(),
           );
@@ -89,7 +93,9 @@ pub fn node_macro_derive(input: TokenStream) -> TokenStream {
     #[napi]
     #[automatically_derived]
     impl #name {
-      pub(crate) fn new_reference(env: napi::Env, #(#arguments)*) -> napi::Result<napi::bindgen_prelude::Reference<Self>> {
+      pub(crate) fn new_reference(env: napi::Env, #(#arguments)*) ->
+          napi::Result<napi::bindgen_prelude::Reference<Self>> {
+
         let inner = Self {
           #(#default_fields)*
           #(#argument_fields)*
@@ -100,13 +106,15 @@ pub fn node_macro_derive(input: TokenStream) -> TokenStream {
       }
 
       #[napi(getter)]
-      pub fn get_parent_element(&self) -> Option<napi::bindgen_prelude::WeakReference<crate::element::Element>> {
-        let parent_node = self.get_parent_node();
+      pub fn get_parent_element(&self) ->
+          Option<napi::bindgen_prelude::WeakReference<crate::element::Element>> {
+
+        let parent_node = self.parent.as_ref();
 
         match parent_node {
           Some(element_or_document) => {
             match (element_or_document) {
-              napi::Either::A(element) => Some(element),
+              napi::Either::A(element) => Some(element.clone()),
               napi::Either::B(_) => None,
             }
           },
@@ -115,8 +123,13 @@ pub fn node_macro_derive(input: TokenStream) -> TokenStream {
       }
 
       #[napi(getter)]
-      pub fn get_parent_node(&self) -> Option<napi::Either<napi::bindgen_prelude::WeakReference<crate::element::Element>, napi::bindgen_prelude::WeakReference<crate::document::Document>>> {
-        let maybe_reference = self.parent.as_ref();
+      pub fn get_parent_node(&self) ->
+          Option<napi::Either<
+            napi::bindgen_prelude::WeakReference<crate::element::Element>,
+            napi::bindgen_prelude::WeakReference<crate::document::Document>
+          >> {
+
+            let maybe_reference = self.parent.as_ref();
 
         maybe_reference.map(|value| match value {
           napi::Either::A(element) => napi::Either::A(element.clone()),
