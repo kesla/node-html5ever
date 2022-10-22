3a37d14dabebd6dadce7ab830600f064e0f13cd8
David Björklund <david.bjorklund@gmail.com>
2022-10-22 12:36:28 +0200

    Revert "merge dts in build.rs"
    This reverts commit d431eab4c944cab74de62be422b407e165aaf809.

--- build.rs
+++ build.rs
@@ -1,31 +1,5 @@
 extern crate napi_build;
 
-use std::fs::File;
-use std::io::prelude::*;
-use std::path::Path;
-
 fn main() {
-  // first build it
   napi_build::setup();
-
-  merge_dts();
-}
-
-fn merge_dts() {
-  let path = Path::new("index.d.ts");
-  let mut output = File::create(&path).unwrap();
-
-  ["types/additional.d.ts", "types/generated.d.ts"]
-    .iter()
-    .for_each(|path| {
-      let path = &Path::new(path);
-      let mut input = File::open(path).unwrap();
-      let mut content = String::new();
-      content.push_str(format!("// {}\n", path.display()).as_str());
-      input.read_to_string(&mut content).unwrap();
-      content = content.trim().to_string();
-      content.push('\n');
-
-      output.write_all(content.as_bytes()).unwrap();
-    });
 }
