f2ef111649301749b7a085fb630e40e9dfda3d0c
David Björklund <david.bjorklund@gmail.com>
2022-12-30 22:19:40 +0100

    format serializer.rs based on copilot suggestions

--- main/src/serializer.rs
+++ main/src/serializer.rs
@@ -2,7 +2,9 @@ use std::collections::VecDeque;
 
 use html5ever::{
     serialize::{
+        Serialize,
         SerializeOpts,
+        Serializer,
         TraversalScope,
     },
     QualName,
@@ -18,20 +20,20 @@ enum SerializeOp {
     Close(QualName),
 }
 
-impl html5ever::serialize::Serialize for Node {
+impl Serialize for Node {
     fn serialize<S>(
         &self,
         serializer: &mut S,
-        traversal_scope: html5ever::serialize::TraversalScope,
+        traversal_scope: TraversalScope,
     ) -> std::io::Result<()>
     where
-        S: html5ever::serialize::Serializer,
+        S: Serializer,
     {
         let mut ops: VecDeque<SerializeOp> = match traversal_scope {
-            html5ever::serialize::TraversalScope::IncludeNode => {
+            TraversalScope::IncludeNode => {
                 VecDeque::from([SerializeOp::Open(self.into())])
             },
-            html5ever::serialize::TraversalScope::ChildrenOnly(_) => {
+            TraversalScope::ChildrenOnly(_) => {
                 if let Node::Element(element) = &self {
                     element
                         .get_all_child_nodes()
@@ -49,42 +51,37 @@ impl html5ever::serialize::Serialize for Node {
 
         while let Some(op) = ops.pop_front() {
             match op {
-                SerializeOp::Open(node) => {
-                    match &node {
-                        ChildNode::Comment(comment) => {
-                            serializer.write_comment(&comment.data)?
-                        },
-                        ChildNode::DocumentType(doc_type) => {
-                            serializer.write_doctype(&doc_type.name)?
-                        },
-                        ChildNode::Element(element) => {
-                            serializer.start_elem(
-                                // TODO: Is this actually copying the data? Need to figure that out
-                                element.name.clone(),
-                                element
-                                    .attributes_wrapper
-                                    .iter()
-                                    .map(|at| (&at.name, &at.value[..])),
-                            )?;
-
-                            ops.push_front(SerializeOp::Close(
-                                element.name.clone(),
-                            ));
-
+                SerializeOp::Open(node) => match &node {
+                    ChildNode::Comment(comment) => {
+                        serializer.write_comment(&comment.data)?
+                    },
+                    ChildNode::DocumentType(doc_type) => {
+                        serializer.write_doctype(&doc_type.name)?
+                    },
+                    ChildNode::Element(element) => {
+                        serializer.start_elem(
+                            element.name.clone(),
                             element
-                                .get_all_child_nodes()
-                                .into_iter()
-                                .rev()
-                                .for_each(|child_node| {
-                                    ops.push_front(SerializeOp::Open(
-                                        child_node,
-                                    ))
-                                });
-                        },
-                        ChildNode::Text(text) => {
-                            serializer.write_text(&text.data)?
-                        },
-                    }
+                                .attributes_wrapper
+                                .iter()
+                                .map(|at| (&at.name, &at.value[..])),
+                        )?;
+
+                        ops.push_front(SerializeOp::Close(
+                            element.name.clone(),
+                        ));
+
+                        element
+                            .get_all_child_nodes()
+                            .into_iter()
+                            .rev()
+                            .for_each(|child_node| {
+                                ops.push_front(SerializeOp::Open(child_node))
+                            });
+                    },
+                    ChildNode::Text(text) => {
+                        serializer.write_text(&text.data)?
+                    },
                 },
                 SerializeOp::Close(name) => serializer.end_elem(name)?,
             }
